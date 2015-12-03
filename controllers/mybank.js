1d5c69e1fab08e1016b9258a0c790433d68a3549
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-12-03 16:40:36 +0000

    View of locations working

--- controllers/mybank.js
+++ controllers/mybank.js
@@ -42,6 +42,42 @@ $( function () {
    });
 });
 
+
+$(function () {
+   $(".viewLocation").click(function () {
+       var url, response, index, li = "";
+
+       url = "http://cs.ashesi.edu.gh/~csashesi/class2016/fredrick-abayie/mobileweb/mybank/php/mybank.php?cmd=view_locations";
+
+       response = sendRequest(url);
+
+       if (response.result === 1) {
+            for (index in response.locations) {
+                li += '<li class="collection-item avatar">';
+                li += '<img src="images/yuna.jpg" alt="" class="circle">';
+                li += '<span class="title">'+response.locations[index].area_name+'</span>';
+                li += '<p>'+response.locations[index].latitude+', '+response.locations[index].longitude+'<br>';
+                li += 'Type, Bank</p>';
+                li += '<a href="https://www.google.com.gh/maps/@5.7549425,-0.2022392,15z?hl=en" class="secondary-content">';
+                li += '<i class="material-icons">location_on</i>';
+                li += '</a></li>';
+            }
+           $("#view").html(li);
+       }
+       //<li class="collection-item avatar">
+       //    <img src="images/yuna.jpg" alt="" class="circle">
+       //    <span class="title">Area</span>
+       //    <p>Lon, Lat <br>
+       //Type, Bank
+       //</p>
+       //<a href="#!" class="secondary-content">
+       //    <i class="material-icons">location_on</i>
+       //    </a>
+       //    </li>
+   });
+});
+
+
 $(document).ready(function() {
     $('select').material_select();
 });
